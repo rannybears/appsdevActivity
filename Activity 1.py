b0c8b8251dc45488779a55a6d840688b23b2cd89
Rann <143052256+rannybears@users.noreply.github.com>
2025-02-06 13:56:46 +0800

    Add files via upload

---
+++ Activity 1.py
@@ -0,0 +1,4 @@
+print("\t\t\t\t\tGreen")
+print("\t\t\tYellow \t\t\tBrown")
+print("\t\tOrange \t\t\t\t\tIndigo")
+print("\tRed \t\t\t\t\t\t\tViolet")
---
+++ Activity 2.py
@@ -0,0 +1,5 @@
+print("CCCCC \tOOOOO \tDDDDD \tEEEEE")
+print("C \t\tO\tO \tD\t D \tE")
+print("C \t\tO\tO \tD\t  D EEE")
+print("C \t\tO\tO \tD\t D \tE")
+print("CCCCC \tOOOOO \tDDDDD \tEEEEE")
