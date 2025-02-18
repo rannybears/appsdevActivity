3a8342e4db59382d8eb0f55a25e5276df088600d
Rann <143052256+rannybears@users.noreply.github.com>
2025-02-18 13:15:08 +0800

    Add files via upload
    Updated Activities 1-4

---
+++ OutputOperation.py
@@ -0,0 +1,14 @@
+# print("Welcome \nto \nPython \"Prgoramming \'! \\!.")
+#
+# name = input("Enter your name: ")
+# age = input("How old are you: ")
+# address = input("Where do you live: ")
+#
+# print(f"My name is {name}, I'm {age} years old. I live in {address}.")
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+#
+# sum = num1 + num2
+#
+# print(f"The sum of {num1} + {num2} =     {sum}")
